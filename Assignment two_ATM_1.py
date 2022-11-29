ea2c74ddb791f3ac581a09aa7d9afdead6094f3b
Sara PG <39550635+pullz6@users.noreply.github.com>
2022-11-29 10:40:16 +0000

    Update Assignment two_ATM_1.py

--- Assignment two_ATM_1.py
+++ Assignment two_ATM_1.py
@@ -11,10 +11,24 @@ import numpy as np
 
 
 def return_two_Dataframes(filename): 
-    df = pd.read_csv(filename, skiprows=(1500))
+    df = pd.read_csv(filename)
     df_t = pd.DataFrame.transpose(df)
     header = df_t.iloc[0].values.tolist()
     df_t.columns = header
-    return df_t
+    return df_t 
 
-print(return_two_Dataframes('API_19_DS2_en_csv_v2_4700503.csv'))
+def return_population(df_all): 
+    df_pop = df_all[df_all['Indicator Name']=='Population, total']
+    df_pop = df_pop.reset_index()
+    return df_pop
+
+def return_Co2Emissions(df_all): 
+    df_pop = df_all[df_all['Indicator Name']=='CO2 emissions from liquid fuel consumption (kt)']
+    df_pop = df_pop.reset_index()
+    return df_pop
+
+
+
+df = pd.read_csv('API_19_DS2_en_csv_v2_4700503.csv')
+#print(return_population(df))
+print(return_Co2Emissions(df))
